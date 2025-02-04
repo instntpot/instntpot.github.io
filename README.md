e3691241a9ce26e66d8d5df1b5a27259c11462dc
instantpot <89133371+instntpot@users.noreply.github.com>
2025-02-03 18:49:16 -0600

    update 2

--- README.md
+++ README.md
@@ -1,10 +1,17 @@
 ### hello this is my website
 
+update 2:
+- added parallax scrolling
+- improved scaling on smaller devices
+- updated most text
+- updated interests
+- changed some colors, size, spacing
+- cleaned up css
+
 update 1:
 - added list of interests
 - added small paragraph below swordsmachines
 - added little spinning heavies
-- added tf2 clip folder link
 - added steam and discord links
 - edited some text
 - changed some spacing and sizing and formatting
--- js/index.js
+++ js/index.js
@@ -1,12 +1,25 @@
 let alerted = false
 
 function toggle_otherstuff() {
-    document.getElementById("otherwrapper").toggleAttribute("disabled")
+    document.getElementsByClassName("otherwrapper")[0].toggleAttribute("disabled")
 }
 
 function check_width() {
-    if (window.innerWidth < 525 && alerted === false) {
-        alert("website starts to get kinda messy with this thin of a viewport")
+    if (window.innerWidth < 325 && alerted === false) {
+        alert("website gets kinda messy with this thin of a screen")
         alerted = true
     }
 }
+
+function parallaxScroll() {
+    document.documentElement.style.backgroundPositionY = `${window.scrollY * 0.8}px`
+}
+
+document.getElementById('enableParallax').addEventListener('change', (event) => {
+    if (!event.target.checked) {
+        removeEventListener("scroll", parallaxScroll);
+    } else {
+        addEventListener("scroll", parallaxScroll);
+    }
+  });
+if (document.getElementById('enableParallax').checked) {addEventListener("scroll", parallaxScroll);}
